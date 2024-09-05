5c73d899712596d4ef16109fafd71ea32eb06ea5
k0honmono817 <mkh990817@gmail.com>
2024-09-05 19:03:53 +0900

    [ADD] | week1

---
+++ week1/Chapter1/1-1_self_study.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+
+void main()
+{
+    int result;
+    result = 123 * 456;
+    printf("%d\n", result);
+}
---
+++ week1/Chapter1/first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main ()
+{
+    int result;
+    result = 100 + 25;
+    printf("%d\n", result);
+}
